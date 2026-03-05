9b8db9b5368ad44722c7de2b60a8de0494bcdb82
mechikuroki <kurokimercedes@gmail.com>
2026-03-05 19:47:42 -0300

    gente

--- gente.py
+++ gente.py
@@ -1,20 +1,25 @@
 import random, math
 nameslist = ["Juli", "Caro", "Nuria"]
-
-def newname(index, name):
-    nameslist.insert(index, name)
-    print(f"eu {nameslist[i]}, querés ir a cenar?")
+def message(name):
+    print(f"eu {name}, querés ir a cenar?")
 
 for i in range(len(nameslist)):
-    print(f"eu, {nameslist[i]}, querés ir a cenar?")
+    message(nameslist[i])
 
 num = random.randint(0, 2)
 print(f"{nameslist[num]} no puede venir")
 nameslist[num] = "Trini"
 print(f"ahora viene {nameslist[num]}")
-print(f"eu, {nameslist[num]}, querés ir a cenar?")
+message(nameslist[num])
 
-newname(0, "Ashton")
-newname(math.ceil(len(nameslist)/2), "Tomi")
-newname(-1, "Emi")
+nameslist.insert(0, "Ashton")
+message(nameslist[0])
+nameslist.insert(math.ceil(len(nameslist)/2), "Tomi")
+message(nameslist[math.ceil(len(nameslist)/2)])
+nameslist.append("Emi")
+message(nameslist[-1])
 
+for _ in range(len(nameslist) - 2):
+    num = random.randint(0, len(nameslist) -1)
+    print(f"{nameslist[num]} no entra!") 
+    nameslist.pop(num)
